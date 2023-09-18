9c82273d70225a854afe92c5887388fcab92781d
Jaeman Kim <daga4242@gmail.com>
2023-09-19 01:59:02 +0900

    chat with websocket

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,26 @@
-console.log('js');
+const messageList = document.querySelector('ul');
+const messageForm = document.querySelector('form');
+
+const socket = new WebSocket(`ws://${window.location.host}`);
+
+socket.addEventListener('open', () => {
+  console.log('Open');
+});
+
+socket.addEventListener('message', (message) => {
+  console.log(message);
+  console.log(`message : ${message.data}`);
+});
+
+socket.addEventListener('close', () => {
+  console.log('close');
+});
+
+function handleSubmit(e) {
+  e.preventDefault();
+  const input = messageForm.querySelector('input');
+  socket.send(input.value);
+  input.value = '';
+}
+
+messageForm.addEventListener('submit', handleSubmit);
--- src/server.js
+++ src/server.js
@@ -16,4 +16,21 @@ const handleListen = () => console.log('Listening on http://localhost:3000');
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
+const sockets = [];
+
+wss.on('connection', (socket) => {
+  sockets.push(socket);
+
+  console.log('open');
+  socket.on('close', () => console.log('close'));
+  socket.on('message', (message) => {
+    sockets.forEach((s) => s.send(`${message}`));
+    console.log(typeof message);
+    console.log(Object.keys(message));
+    console.log(Object.values(message));
+    console.log(`message : ${message}`);
+  });
+  socket.send('hello');
+});
+
 server.listen(3000, handleListen);
